0def34441b528bbc214d6ce3660de7cf0f8e413a
Omri <Omri@Summoners-Rift.local>
2018-02-04 20:36:17 +0200

    fix for old browsers

--- main.js
+++ main.js
@@ -1,38 +1,79 @@
-const END_TIME = 1518562800000; // February 13, 6:00PM 2018
+var END_TIME = 1518562800000; // February 13, 6:00PM 2018
 
 function breakdownTimeLeft(timeLeft) {
-  let hours;
-  let minutes;
-  let seconds;
+  var hours;
+  var minutes;
+  var seconds;
 
   seconds = Math.floor(timeLeft / 1000);
   minutes = Math.floor(seconds / 60);
   seconds %= 60;
   hours = Math.floor(minutes / 60);
   minutes %= 60;
-  const days = Math.floor(hours / 24);
+  var days = Math.floor(hours / 24);
   hours %= 24;
 
   return {
-    days, hours, minutes, seconds,
+    days: days, hours: hours, minutes: minutes, seconds: seconds,
   };
 }
 
+
 function zeroPad(num) {
-  return num >= 10 ? `${num}` : `0${num}`;
+  var str =  num >= 10 ? num.toString() : "0" + num;
+
+  return str;
 }
 
 function updateTimers(data) {
   if (!data) {
-    document.querySelectorAll('.days').forEach((e) => { e.innerText = '0'; });
-    document.querySelectorAll('.hours').forEach((e) => { e.innerText = '00'; });
-    document.querySelectorAll('.minutes').forEach((e) => { e.innerText = '00'; });
-    document.querySelectorAll('.seconds').forEach((e) => { e.innerText = '00'; });
+    var d = document.querySelectorAll('.days');
+    for (var i=0; i< d.length; i++) {
+      var e = d[i];
+      e.innerText = '0'; 
+    }
+
+    var h = document.querySelectorAll('.hours');
+    for (var i=0; i< h.length; i++) {
+      var e = h[i];
+      e.innerText = '00';
+    }
+
+    var m = document.querySelectorAll('.minutes');
+    for (var i=0; i< m.length; i++) {
+      var e = m[i];
+      e.innerText = '00';
+    }
+    
+    var s = document.querySelectorAll('.seconds');
+    for (var i=0; i< s.length; i++) {
+      var e = s[i];
+      e.innerText = '00';
+    }
   } else {
-    document.querySelectorAll('.days').forEach((e) => { e.innerText = `${data.days}`; });
-    document.querySelectorAll('.hours').forEach((e) => { e.innerText = zeroPad(data.hours); });
-    document.querySelectorAll('.minutes').forEach((e) => { e.innerText = zeroPad(data.minutes); });
-    document.querySelectorAll('.seconds').forEach((e) => { e.innerText = zeroPad(data.seconds); });
+    var d = document.querySelectorAll('.days');
+    for (var i=0; i< d.length; i++) {
+      var e = d[i];
+      e.innerText = data.days.toString(); 
+    }
+
+    var h = document.querySelectorAll('.hours');
+    for (var i=0; i< h.length; i++) {
+      var e = h[i];
+      e.innerText = zeroPad(data.hours); 
+    }
+
+    var m = document.querySelectorAll('.minutes');
+    for (var i=0; i< m.length; i++) {
+      var e = m[i];
+      e.innerText = zeroPad(data.minutes); 
+    }
+    
+    var s = document.querySelectorAll('.seconds');
+    for (var i=0; i< s.length; i++) {
+      var e = s[i];
+      e.innerText = zeroPad(data.seconds); 
+    }
   }
 }
 
@@ -40,11 +81,20 @@ function countdownLoop() {
   if (END_TIME - Date.now() <= 0) {
     return updateTimers();
   }
-
-  return setTimeout(() => {
+  
+  return setTimeout(function timeout() {
     updateTimers(breakdownTimeLeft(END_TIME - Date.now()));
-    countdownLoop();
+    countdownLoop();  
   }, 1000);
 }
 
-document.addEventListener('DOMContentLoaded', countdownLoop, false);
+
+function init() {
+  if (document.readyState !== 'loading') {
+    countdownLoop();
+  } else {
+    setTimeout(init, 100);
+  }
+}
+
+init();
